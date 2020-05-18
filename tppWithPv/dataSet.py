26f84e555ce5955b2b12ff9e8e823b5a6c4af8e6
Shouta Ukai <shouta.ukai@gmail.com>
2020-05-18 10:30:10 +0900

    Modified the pv-dataset class in order to load a given csv file
    Modified the pv-dataset class in order to load a given csv file

--- tppWithPv/dataSet.py
+++ tppWithPv/dataSet.py
@@ -50,29 +50,49 @@ class TestEventDataSet():
 
 class TestPvDataSet():
 
-    _uniqueInstance = {}
+    _uniqueInstance = None
 
-    def __init__(self, Npv):
+    def __init__(self):
 
-        self.Npv = Npv
+        
+        with open("dataPv.csv") as fp:
+            txt = fp.read()
+
+        dataRaw = []
+        
+        # the first line is supposed to be the header line.
+        for line in txt.split("\n")[1:]:
+            if len(line.rstrip()) > 0:
+                cells = line.rstrip().split(",") # each cells contain 0 or 1, which represent the occurrence of a relative event.
+                dataRaw.append([float(cell) for cell in cells])
+        dataRaw = np.array(dataRaw, dtype=np.float32) # (*, Npv)
+        
+        data = (dataRaw - np.nanmean(dataRaw, axis=0))/np.nanstd(dataRaw, axis=0) # (*, Npv)
+        
+        (Nsample, Npv) = data.shape
+        print("A data set with the dimension (Nsample=%d, Npv=%d) has been loaded, successfully." % (Nsample, Npv))
+
+        self.data = data # (Nsample, Npv)
+
 
     @classmethod
     def getInstance(cls, Npv):
-        if not Npv in cls._uniqueInstance:
-            cls._uniqueInstance[Npv] = super().__new__(cls)
-            cls._uniqueInstance[Npv].__init__(Npv)
-        return cls._uniqueInstance[Npv]
+        
+        if cls._uniqueInstance is None:
+            cls._uniqueInstance = super().__new__(cls)
+            cls._uniqueInstance.__init__()
+        return cls._uniqueInstance
 
     def getNsample(self):
-        N = 2**10
-        return N
+        return self.data.shape[0]
+    
+    def getNpv(self):
+        return self.data.shape[1]
 
     def getSlice(self, idx):
 # idx: (...)
-        pvDataBatch = np.random.randn(*idx.shape, self.Npv) # (..., Npv)
-        return pvDataBatch
+        return self.data[idx, :] # (..., Npv)
 
     def getAvailableIndex(self):
-        idxAvailable = np.arange(self.getNsample()) # (*,)
-        return idxAvailable # (*,)
-
+        idxAvailable = np.where(~np.any(np.isnan(self.data), axis=1))[0] # (*,)
+        return idxAvailable
--- tppWithPv/test_dataset.py
+++ tppWithPv/test_dataset.py
@@ -5,7 +5,7 @@ Created on 2020/05/18
 '''
 import unittest
 import numpy as np
-from tppWithPv.dataSet import TestEventDataSet
+from tppWithPv.dataSet import TestEventDataSet, TestPvDataSet
 
 
 class Test(unittest.TestCase):
@@ -13,19 +13,36 @@ class Test(unittest.TestCase):
 
     def test001(self):
         
-        nSample, nDelta = np.random.randint(1, 2**5, size=(2,))
-        dataRaw = np.random.randint(2, size=(nSample, nDelta))
-        filePath = "./dataDelta.csv"
-        with open(filePath, "w") as fp:
-            fp.write("#\n")
-            for row in dataRaw:                
-                fp.write(",".join([str(cell) for cell in row]) + "\n")
+        for _ in range(2**7):
+            nSample, nDelta = np.random.randint(1, 2**5, size=(2,))
+            dataRaw = np.random.randint(2, size=(nSample, nDelta))
+            filePath = "./dataDelta.csv"
+            with open(filePath, "w") as fp:
+                fp.write("#\n")
+                for row in dataRaw:                
+                    fp.write(",".join([str(cell) for cell in row]) + "\n")
+            
+            ds = TestEventDataSet()
+            
+            assert ds.getNdelta() == nDelta
+            assert ds.getNsample() == nSample
         
-        ds = TestEventDataSet()
         
-        assert ds.getNdelta() == nDelta
-        assert ds.getNsample() == nSample
+    def test002(self):
 
+        for _ in range(2**7):        
+            nSample, nPv = np.random.randint(2, 2**5, size=(2,))
+            dataRaw = np.random.randn(nSample, nPv)
+            filePath = "./dataPv.csv"
+            with open(filePath, "w") as fp:
+                fp.write("#\n")
+                for row in dataRaw:                
+                    fp.write(",".join([str(cell) for cell in row]) + "\n")
+            
+            ds = TestPvDataSet()
+            
+            assert ds.getNpv() == nPv
+            assert ds.getNsample() == nSample
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.test001']
