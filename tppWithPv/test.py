ec46acd6da6bbd8c95c77d2c8fd9f6c58a7743ab
Shouta Ukai <shouta.ukai@gmail.com>
2020-05-19 18:12:53 +0900

    Modified the test code to adapt to the fix of specifications

--- tppWithPv/test.py
+++ tppWithPv/test.py
@@ -11,6 +11,7 @@ from tppWithPv.builderTpp import BuilderTpp
 from tppWithPv.dataSet import TestPvDataSet, TestEventDataSet
 from tppWithPv.environmentTpp import EventDataFeederWithPv
 from tppWithPv.trainerTpp import TrainerTppMLE
+import shutil
 
 
 class TestCase(unittest.TestCase):
@@ -21,18 +22,18 @@ class TestCase(unittest.TestCase):
         if not os.path.exists('./tmp'):
             os.mkdir("./tmp")
         
-        nSample = 2**10
+        nSample = 2**6
         nDelta, nPv = np.random.randint(1, 2**2, size=(2,))
         dataRaw = np.random.randint(2, size=(nSample, nDelta))
-        filePath = "./dataDelta.csv"
-        with open(filePath, "w") as fp:
+        eventDataFilePath = "./tmp/dataDelta.csv"
+        with open(eventDataFilePath, "w") as fp:
             fp.write("#\n")
             for row in dataRaw:                
                 fp.write(",".join([str(cell) for cell in row]) + "\n")
                 
         dataRaw = np.random.randn(nSample, nPv)
-        filePath = "./dataPv.csv"
-        with open(filePath, "w") as fp:
+        pvDataFilePath = "./tmp/dataPv.csv"
+        with open(pvDataFilePath, "w") as fp:
             fp.write("#\n")
             for row in dataRaw:                
                 fp.write(",".join([str(cell) for cell in row]) + "\n")
@@ -40,6 +41,14 @@ class TestCase(unittest.TestCase):
         cls.nSample = nSample
         cls.nPv = nPv
         cls.nDelta = nDelta
+        cls.eventDataFilePath = eventDataFilePath
+        cls.pvDataFilePath = pvDataFilePath
+        
+    @classmethod
+    def tearDownClass(cls):
+        if os.path.exists('./tmp'):
+            shutil.rmtree("./tmp")
+
 
 
     def test_001(self):
@@ -68,8 +77,8 @@ class TestCase(unittest.TestCase):
 
     def test_002(self):
 
-        pvDataSet = TestPvDataSet.getInstance()
-        eventDataSet = TestEventDataSet.getInstance()
+        pvDataSet = TestPvDataSet.getInstance(TestCase.pvDataFilePath)
+        eventDataSet = TestEventDataSet.getInstance(TestCase.eventDataFilePath)
         for ds in [pvDataSet, eventDataSet]:
             N = ds.getNsample()
             idx = ds.getAvailableIndex()
@@ -85,7 +94,7 @@ class TestCase(unittest.TestCase):
                 , size=(2,)
                 )
 
-            environment = EventDataFeederWithPv(Nbatch , Nseq)
+            environment = EventDataFeederWithPv(TestCase.eventDataFilePath, TestCase.pvDataFilePath, Nbatch , Nseq)
 
             for idx in environment.iterate():
                 assert idx.shape == (Nbatch,)
@@ -103,7 +112,7 @@ class TestCase(unittest.TestCase):
                 , high=2**3
                 , size=(3,))
 
-            environment = EventDataFeederWithPv(Nbatch , Nseq)
+            environment = EventDataFeederWithPv(TestCase.eventDataFilePath, TestCase.pvDataFilePath, Nbatch , Nseq)
             
             agent = AgentHawkesWithPv(TestCase.nDelta, Nh, TestCase.nPv)
 
@@ -113,9 +122,19 @@ class TestCase(unittest.TestCase):
 
     def test_005(self):
 
-        history = History("./test.db")
-        builder = BuilderTpp(history)
-        builder.build()
+        for _ in range(2**4):
+            Nseq, Nbatch, Nh,  = np.random.randint(low=1
+                , high=2**3
+                , size=(3,))
+
+            environment = EventDataFeederWithPv(TestCase.eventDataFilePath, TestCase.pvDataFilePath, Nbatch , Nseq, preprocess="Differential", tau = 10)
+            
+            agent = AgentHawkesWithPv(TestCase.nDelta, Nh, TestCase.nPv)
+
+            Nepoch = 2
+            trainer = TrainerTppMLE(agent, environment, Nepoch)
+            trainer.train()
+
 
 if __name__ == "__main__":
     unittest.main()
