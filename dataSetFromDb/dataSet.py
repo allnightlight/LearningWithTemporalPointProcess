8e47c534042848376f5cafd1dc684ca6642a1552
Shouta Ukai <shouta.ukai@gmail.com>
2020-06-16 16:08:05 +0900

    Added the dataset loading from a database

---
+++ dataSetFromDb/dataSet.py
@@ -0,0 +1,83 @@
+
+import numpy as np
+import sqlite3
+import traceback
+import json
+
+
+class DataSetFromDb():
+
+    _uniqueInstance = {}
+
+    def __init__(self, dbFilePath, tags, period):
+        # tags = ["PV0001", "PV0002", ...]
+        # period = (t0, t1), t0, t1 as datetime
+        
+        print("The data will be loaded from the following DB: %s" % dbFilePath
+              + "\n with the given tags: %s" % ",".join(tags)
+              + "\n with the given period: %s - %s" % (period[0], period[1])
+              )
+
+        # This SQL command will extract the dataset with the given tags: "tags"
+        # and with the timestamp between the given time period: "period",
+        # omitting the rows which contain NULL in a column or some columns.
+        # Note, the order alongside tag does not meet with the given tags,
+        # though, it's restored later.
+        sql = """
+Select 
+    d.value
+    From DataTable d
+        Where d.tag in ({0})
+        And timestamp >= ?
+        And timestamp < ?
+    Order by d.timestamp_id, d.tag
+""".format(",".join(map(lambda xx: '"%s"' % xx, tags)))
+
+        conn = None
+        data = None
+        try:
+            conn = sqlite3.connect(dbFilePath, detect_types = sqlite3.PARSE_COLNAMES|sqlite3.PARSE_DECLTYPES)
+            cur = conn.cursor()
+            cur.execute(sql, period)        
+            data = np.array(cur.fetchall(), dtype=np.float32).reshape(-1, len(tags)) # (nSample, nTag)
+        except:
+            traceback.print_exc()
+            data = None
+        finally:
+            if conn is not None:                
+                conn.close()
+        
+        assert data is not None, "FAILED TO LOADING DATA FROM THE GIVEN DB: %s" % dbFilePath
+        self.data = data
+        
+        self.idxAvailable = np.where(~np.any(np.isnan(data), axis=-1))[0] 
+        
+        self.nSample = len(self.idxAvailable)
+        assert self.nSample > 0, "NO AVAILABLE SAMPLE EXISTS IN THE GIVEN PAIR OF PERIOD AND TAGS."
+        
+        self.nTag = len(tags)
+        assert self.nTag == self.data.shape[1], "SOME GIVEN TAGS COULD NOT BE FOUND IN THE GIVEN DB."
+        
+        self.data = data[:, np.argsort(np.argsort(tags))]
+        
+    @classmethod
+    def getInstance(cls, **args):        
+        key = json.dumps(args)        
+        if not key in cls._uniqueInstance:
+            cls._uniqueInstance[key] = super().__new__(cls)
+            cls._uniqueInstance[key].__init__(**args)
+        return cls._uniqueInstance[key]
+
+    def getNsample(self):
+        return self.nSample
+    
+    def getTag(self):
+        return self.nTag
+
+    def getSlice(self, idx):           
+# idx: (...)
+        return self.data[idx, :] # (..., nTag)
+
+    def getAvailableIndex(self):
+        return self.idxAvailable.copy()
+    
---
+++ dataSetFromDb/environmentFromDb.py
@@ -0,0 +1,102 @@
+
+import sqlite3
+import traceback
+
+from dataSet import DataSetFromDb
+from environment import IEnvironment
+from factory import EnvironmentFactory
+import numpy as np
+
+
+class EnvironmentFactoryFromDb(EnvironmentFactory):
+
+    def initialize(self, classname, **args):
+        return eval(classname)(**args)
+
+
+class DataFeederFromDb(IEnvironment):
+
+    def __init__(self, dbFilePath, period, pv_tags, pv_preprocesses, ev_tags, Nbatch, Nseq):
+        super().__init__()
+
+        sql = """
+Select 
+    Tag.name
+    From Tag
+    Where Tag.id in (Select
+        p.tag_id
+        From PreprocessedTag p
+            Join PreprocessMethod p2
+                On p.preprocess_method_id = p2.id
+            Join Tag 
+                On Tag.id = p.source_tag_id
+        Where Tag.name = ?
+        And p2.name = ? 
+        Limit 1)
+    Limit 1
+"""
+
+        pv_tags_all = None
+        try:
+            conn = sqlite3.connect(dbFilePath, detect_types = sqlite3.PARSE_COLNAMES|sqlite3.PARSE_DECLTYPES)
+            cur = conn.cursor()
+            pv_tags_all = []
+            for source_tag, preprocesses in zip(pv_tags, pv_preprocesses):
+                for preprocess in preprocesses:
+                    if preprocess == "None":
+                        pv_tags_all.append(source_tag)
+                    else:
+                        cur.execute(sql, (source_tag, preprocess,))
+                        tag, = cur.fetchone()
+                        pv_tags_all.append(tag)
+        except:
+            traceback.print_exc()
+            pv_tags_all = None
+        finally:
+            conn.close()
+            
+        assert pv_tags_all is not None, "FAILED TO LOADING DATA FROM THE GIVEN DB: %s" % dbFilePath
+        
+        all_tags = list(ev_tags) + list(pv_tags_all)
+        self.ev_idx = [k1 for k1 in range(len(ev_tags))]
+        self.pv_idx = [k1 + len(ev_tags) for k1 in range(len(pv_tags_all))]
+        self.dataSet = DataSetFromDb(dbFilePath, all_tags, period)
+        self.Nbatch = Nbatch
+        self.Nseq = Nseq
+        
+        assert self.dataSet.getNsample() > Nseq
+
+    def getAvailableIndex(self):
+        idxAvailablePrimitive = self.dataSet.getAvailableIndex()
+        idxAvailable = [ i for i in idxAvailablePrimitive
+            if np.all(np.isin(np.arange(i+1-self.Nseq, i+1), idxAvailablePrimitive)) ]
+        return idxAvailable # (almost N-Nseq+1,)
+
+    def iterate(self):
+        idxAvailable = self.getAvailableIndex() # (almost N-Nseq+1,)
+        Navailable = len(idxAvailable)
+        for _ in range(Navailable//self.Nbatch):
+            yield np.random.choice(idxAvailable, 
+                size = (self.Nbatch,)) # (Nbatch,)
+
+    def getBatchData(self, idx):
+# idx: (Nbatch,)
+        idxWithSeq = idx + np.arange(1-self.Nseq, 1).reshape(-1, 1)
+# (Nseq, Nbatch)
+        dataBatch= self.dataSet.getSlice(idxWithSeq) 
+        # (Nseq, Nbatch, Ndelta + nPv)
+        
+        eventDataBatch = dataBatch[..., self.ev_idx] # (nSeq, nBatch, Ndelta)
+        pvDataBatch = dataBatch[..., self.pv_idx] # (nSeq, nBatch, nPv)
+        
+        return eventDataBatch, pvDataBatch
+
+    def getTrainData(self):
+        idxAvailable = self.getAvailableIndex() # (almost N-Nseq+1,)
+        return self.getBatchData(idxAvailable) 
+
+    def getNpv(self):
+        return len(self.pv_idx)
+
+    def getNdelta(self):
+        return len(self.ev_idx)
