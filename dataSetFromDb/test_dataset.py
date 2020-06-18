ce9ff1609777075526e0009f7dce2a2f3b0403ac
Shouta Ukai <shouta.ukai@gmail.com>
2020-06-18 10:43:28 +0900

    Added the test codes

--- dataSetFromDb/test_dataset.py
+++ dataSetFromDb/test_dataset.py
@@ -3,7 +3,7 @@ Created on 2020/06/13
 
 @author: ukai
 '''
-from datetime import timedelta
+from datetime import timedelta, datetime
 import unittest
 
 from dataSet import DataSetFromDb
@@ -15,7 +15,8 @@ class Test(unittest.TestCase):
     def test001(self):
         
         tags = ["PV%04d" % (k1+1)  for k1 in range(10)]
-        period = ('2020-06-12 16:20:00', '2020-06-12 17:05:00')
+        period = (datetime.strptime("2020/6/12 16:20", "%Y/%m/%d %H:%M")
+                  , datetime.strptime("2020/6/12 17:05", "%Y/%m/%d %H:%M"))
         samplingIntervalMinute = 15
         
         DataSetFromDb("./testDb.sqlite", tags, period, samplingIntervalMinute)
@@ -29,6 +30,13 @@ class Test(unittest.TestCase):
         
         DataSetFromDb.getInstance(**args)
         ds = DataSetFromDb.getInstance(**args)
+        
+        timestampAvailable = ds.getAvailableTimestamp()
+        assert len(timestampAvailable) > 0
+        for s,t in zip(timestampAvailable[:-1], timestampAvailable[1:]):
+            dt = t-s
+            assert dt.total_seconds() == samplingIntervalMinute * 60
+        
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.test001']
--- dataSetFromDb/test_environment.py
+++ dataSetFromDb/test_environment.py
@@ -3,6 +3,7 @@ Created on 2020/06/14
 
 @author: ukai
 '''
+from datetime import datetime
 import unittest
 
 from environmentFromDb import EnvironmentFactoryFromDb
@@ -15,7 +16,14 @@ class Test(unittest.TestCase):
 
         args = dict(         
             dbFilePath = "./testDb.sqlite"
-            , period = ("2020-06-12 17:05:00", "2020-06-13 14:45:00")
+            , period_train = (
+                datetime.strptime("2020/6/12 17:05", "%Y/%m/%d %H:%M")
+                , datetime.strptime("2020/6/13 14:45", "%Y/%m/%d %H:%M")
+                )
+            , period_test= (
+                datetime.strptime("2020/6/13 17:05", "%Y/%m/%d %H:%M")
+                , datetime.strptime("2020/6/14 14:45", "%Y/%m/%d %H:%M")
+                ) 
             , pv_tags = ("PV0006", "PV0016")
             , pv_preprocesses = (
                 ("None", "Preprocess0001")
@@ -33,6 +41,8 @@ class Test(unittest.TestCase):
         for idx in environment.iterate():
             E, Pv = environment.getBatchData(idx) # (Nseq, *, Ndelta)
 
+        E, Pv = environment.getTrainData()
+        E, Pv = environment.getTestData()
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.test001']
